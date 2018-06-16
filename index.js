0e495d91923911c440629aaaf0f363c9eb5f4377
Seth Balodi <sethbalodi@Exantes-MacBook-Pro.local>
2018-06-15 20:14:39 -0400

    yahyahyah

---
+++ index.js
@@ -0,0 +1,15 @@
+Star[] starts = new Star[100];
+
+
+void setup() {
+    size(400, 400);
+    for (var i = 0;Œ i < stars.length; i++) {
+        stars[i] = new Star();
+    }
+}
+
+void draw() {
+    for (var i = 0; i < stars.length; i++) {
+        stars[i] = new Star();
+    }
+}
---
+++ star.js
@@ -0,0 +1,12 @@
+class Star {
+    float x;
+    float y;
+    float z;
+
+    star() {
+        // width of the window and height of the window
+        x = random(0, width);
+        y = random(0, height);
+        z = random(0, width);
+    }
+}
